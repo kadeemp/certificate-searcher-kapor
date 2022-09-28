3767a04df2d507c783ac32dd64085d1ae0d98e95
Kadeem Palacios <kadeempalacios@gmail.com>
2022-09-27 18:57:12 -0700

    Implemented File Uploads using cloudstore & storage
    reminder to remove collect.js. It is not being used.
    Bugs to fix:
    Id assignment
    First insert is in the wrong place. Possible overwrite?   first id should 1. its 5 on record.
    
    features to add:
    clear forms on submit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,59 @@
 import './App.css';
 import Profile from './Profile.jsx'
+import { useState, useEffect } from "react";
+import { db } from './firebase.js'
+import {
+  collection,
+  getDocs,
+  addDoc,
+  updateDoc,
+  deleteDoc,
+  doc,
+} from "firebase/firestore";
+import {
+  ref,
+  uploadBytes,
+  getDownloadURL,
+  list,
+  listAll,
+} from "firebase/storage";
+import { storage } from "./firebase";
+import { v1 } from "uuid";
+import collect from 'collect.js';
 
 function App() {
+
+const [users, setUsers] = useState([])
+const profilesCollectionRef = collection(db,"profiles")
+
+
+const [profiles, setProfiles] = useState([]);
+const [newName, setNewName] = useState("")
+const [newfName, setNewfName] = useState("")
+const [newlName, setNewlName] = useState("")
+const [id, setNewid] = useState("")
+const [bundleId, setNewBundleId] = useState("")
+
+const createProfile = async () => {
+  console.log("profile created, \n profile count is : /${profiles.}");
+  addDoc(profilesCollectionRef, { certificateBundleID: v1() ,fname:newfName ,lname:newlName,name:newName })
+}
+
+
+useEffect(() => {
+  console.log(profiles,1);
+getDocs(profilesCollectionRef).then((response) => {
+  response.docs.forEach((profile) => {
+    profiles.push(profile)
+  });
+  console.log(profiles, 2);
+  setProfiles(profiles)
+});
+
+}, []);
+
+
+
   return (
     <div className="App">
       <header className="App-header">
--- src/Profile.jsx
+++ src/Profile.jsx
@@ -1,25 +1,114 @@
 import React from 'react';
+import {
+  collection,
+  getDocs,
+  addDoc,
+  updateDoc,
+  deleteDoc,
+  doc,
+} from "firebase/firestore";
+import {
+  ref,
+  uploadBytes,
+  getDownloadURL
+} from "firebase/storage";
+import { db } from './firebase.js'
+import { v4 } from "uuid";
+import { storage } from "./firebase";
 
-
+import { useState } from "react";
 function Profile(props) {
 
+  const profilesCollectionRef = collection(db,"profiles")
+  const certificatesCollectionRef = collection(db,"certificates")
+
+
+  const [certificateUpload, setNewCertificateUpload] = useState(null)
+  const [newName, setNewName] = useState("")
+  const [newfName, setNewfName] = useState("")
+  const [newlName, setNewlName] = useState("")
+
 function handleClick(event) {
-  
+event.preventDefault()
+const date = new Date();
+
+let day = date.getDate();
+let month = date.getMonth() + 1;
+let year = date.getFullYear();
+let currentDate = `${month}-${day}-${year}`
+let count = 0
+let id = 0
+let idStr = ""
+let  certificateBundleID = v4()
+
+//get profiles.
+ getDocs(certificatesCollectionRef).then((response) => {
+console.log(`OG count is ${count}`)
+     count = response.docs.length - 1
+
+     if (count < 0 ) {
+       count = 0
+     }
+let hasUploaded = false
+console.log(`returned count is ${count}, docs ${response.docs.length}`);
+for (let i = 0; i < 5; i++) {
+
+        id = (count * 5) + 1 + i
+console.log(`LOOP:  count is ${count}, id is ${id}`);
+        if (id < 9)  {
+        idStr = "#00000" + id
+      } else if (id < 99) {
+          idStr = "#0000" + id
+        } else if (id < 999) {
+          idStr = "#000" + id
+        }
+        console.log(`idStr = ${idStr}`);
+
+  const certificatesListRef = ref(storage, `cert-pdfs/${idStr}.pdf`);
+        if (hasUploaded == false) {
+          uploadBytes(certificatesListRef, certificateUpload).then((snapshot) => {
+            getDownloadURL(snapshot.ref).then((url) => {
+              if (url !== null) {
+
+                addDoc(certificatesCollectionRef, { certificateBundleID:certificateBundleID, id:idStr, level:1, date:currentDate,link:url, fname:newfName, lname:newlName})
+
+
+              }
+            });
+          })
+          console.log(`V1 hasUploaded is ${hasUploaded}`);
+          hasUploaded = true
+          console.log(`V2 hasUploaded is ${hasUploaded}`);
+
+
+        } else {
+          console.log("THIS IS GETTING RAN");
+          addDoc(certificatesCollectionRef, { certificateBundleID:certificateBundleID, id:idStr, level:1, date:"",link:"", fname:newfName, lname:newlName})
+
+        }
+}
+});
 }
 
   return (
-
 <div>
     <div>
-        <h4> {props.fname} </h4>
+        <input type="text" onChange={(event) => {
+          setNewfName(event.target.value)
+        }}/>
     </div>
     <div>
-        <h4> {props.lname} </h4>
+        <input type="text"  onChange={(event) => {
+          setNewlName(event.target.value)
+        }}/>
+    </div>
+    <div>
+        <input type="file" onChange={(event) => { setNewCertificateUpload(event.target.files[0]); }}/>
     </div>
     <div>
 
     </div>
-    <button>Submit</button>
+    <button onClick={handleClick} >Submit</button>
     </div>
 
   );
