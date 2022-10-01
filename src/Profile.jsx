7e12587dd1be8f9b5bf25c3e057300e1de2a2c2b
Kadeem Palacios <kadeempalacios@gmail.com>
2022-09-30 17:15:53 -0700

    Certificate addition bug fixed. Certificate deletion  added

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -21,94 +21,138 @@ function Profile(props) {
 
   const profilesCollectionRef = collection(db,"profiles")
   const certificatesCollectionRef = collection(db,"certificates")
-
-
   const [certificateUpload, setNewCertificateUpload] = useState(null)
   const [newName, setNewName] = useState("")
   const [newfName, setNewfName] = useState("")
   const [newlName, setNewlName] = useState("")
 
-function handleClick(event) {
-event.preventDefault()
-const date = new Date();
+  function reloadPage() {
+    window.location.reload(false)
+  }
+  function nukeDatabase() {
 
-let day = date.getDate();
-let month = date.getMonth() + 1;
-let year = date.getFullYear();
-let currentDate = `${month}-${day}-${year}`
-let count = 0
-let id = 0
-let idStr = ""
-let  certificateBundleID = v4()
+          getDocs(profilesCollectionRef).then((response) => {
+response.docs.forEach((item) => {
+   deleteDoc(doc(db, "profiles", `${item.id}`));
+}); })
 
-//get profiles.
- getDocs(certificatesCollectionRef).then((response) => {
-console.log(`OG count is ${count}`)
-     count = response.docs.length - 1
-
-     if (count < 0 ) {
-       count = 0
-     }
-let hasUploaded = false
-console.log(`returned count is ${count}, docs ${response.docs.length}`);
-for (let i = 0; i < 5; i++) {
-
-        id = (count * 5) + 1 + i
-console.log(`LOOP:  count is ${count}, id is ${id}`);
-        if (id < 9)  {
-        idStr = "#00000" + id
-      } else if (id < 99) {
-          idStr = "#0000" + id
-        } else if (id < 999) {
-          idStr = "#000" + id
-        }
-        console.log(`idStr = ${idStr}`);
-
-  const certificatesListRef = ref(storage, `cert-pdfs/${idStr}.pdf`);
-        if (hasUploaded == false) {
-          uploadBytes(certificatesListRef, certificateUpload).then((snapshot) => {
-            getDownloadURL(snapshot.ref).then((url) => {
-              if (url !== null) {
-
-                addDoc(certificatesCollectionRef, { certificateBundleID:certificateBundleID, id:idStr, level:1, date:currentDate,link:url, fname:newfName, lname:newlName})
-
-
-              }
-            });
-          })
-          console.log(`V1 hasUploaded is ${hasUploaded}`);
-          hasUploaded = true
-          console.log(`V2 hasUploaded is ${hasUploaded}`);
-
-
-        } else {
-          console.log("THIS IS GETTING RAN");
-          addDoc(certificatesCollectionRef, { certificateBundleID:certificateBundleID, id:idStr, level:1, date:"",link:"", fname:newfName, lname:newlName})
-
-        }
-}
+getDocs(certificatesCollectionRef).then((response) => {
+response.docs.forEach((item) => {
+deleteDoc(doc(db, "certificates", `${item.id}`));
 });
+})
+}
+
+function handleClick(event) {
+      event.preventDefault()
+      const date = new Date();
+
+      let day = date.getDate();
+      let month = date.getMonth() + 1;
+      let year = date.getFullYear();
+      let currentDate = `${month}-${day}-${year}`;
+      let count = 0;
+      let id = 0;
+      let idStr = "";
+      let  certificateBundleID = v4();
+
+      //get profiles.
+      getDocs(profilesCollectionRef).then((response) => {
+          count = response.docs.length
+
+          if (count < 0 ) {
+            count = 0
+          } else if (count == 0) {
+            count = 0
+          } else {
+          }
+
+          console.log(`response is ${response.docs}`);
+          console.log(`count is ${count}`);
+      addDoc(profilesCollectionRef,{dateAdded:currentDate, certificateBundleID:certificateBundleID,fname:newfName,lname:newlName, level:1})
+
+             id = (count * 5) + 1
+
+             if (id <= 9)  {
+             idStr = "#00000" + id
+           } else if (id <= 99) {
+               idStr = "#0000" + id
+             } else if (id <= 999) {
+               idStr = "#000" + id
+             }
+             // console.log(`idStr = ${idStr}`);
+       const certificatesListRef = ref(storage, `cert-pdfs/${idStr}.pdf`);
+       if (certificateUpload == null ) {
+
+         for (let i = 0; i < 5; i++) {
+
+                 if (id <= 9)  {
+                 idStr = "#00000" + id
+                 } else if (id <= 99) {
+                   idStr = "#0000" + id
+                 } else if (id <= 999) {
+                   idStr = "#000" + id
+                 }
+                 console.log(` Outer idStr:${idStr}`);
+         addDoc(certificatesCollectionRef, { certificateBundleID:certificateBundleID, id:idStr, level:0, date:"",link:"", fname:newfName, lname:newlName})
+         id = id + 1
+       }
+       // setTimeout(function() { reloadPage(); }, 1500);
+       } else {
+        console.log("certificate uploaded");
+         uploadBytes(certificatesListRef, certificateUpload).then((snapshot) => {
+           getDownloadURL(snapshot.ref).then((url) => {
+              console.log("url returned");
+                              if (id <= 9)  {
+                                idStr = "#00000" + id
+                                } else if (id <= 99) {
+                                  idStr = "#0000" + id
+                                } else if (id <= 999) {
+                                  idStr = "#000" + id
+                                }
+                                console.log(` first idStr is ${idStr}`);
+             addDoc(certificatesCollectionRef, { certificateBundleID:certificateBundleID, id:idStr, level:1, date:currentDate,link:url, fname:newfName, lname:newlName})
+             for (let i = 1; i < 5; i++) {
+                  id = id + 1
+                     if (id <= 9)  {
+                     idStr = "#00000" + id
+                     } else if (id <= 99) {
+                       idStr = "#0000" + id
+                     } else if (id <= 999) {
+                       idStr = "#000" + id
+                     }
+                     console.log(` Outer idStr:${idStr}`);
+             addDoc(certificatesCollectionRef, { certificateBundleID:certificateBundleID, id:idStr, level:0, date:"",link:"", fname:newfName, lname:newlName})
+
+           }
+           })
+         })
+
+      }
+
+      })
 }
 
   return (
 <div>
     <div>
-        <input type="text" onChange={(event) => {
+        <input id="fname" type="text" onChange={(event) => {
           setNewfName(event.target.value)
         }}/>
     </div>
     <div>
-        <input type="text"  onChange={(event) => {
+        <input id="lname" type="text"  onChange={(event) => {
           setNewlName(event.target.value)
         }}/>
     </div>
     <div>
-        <input type="file" onChange={(event) => { setNewCertificateUpload(event.target.files[0]); }}/>
+        <input id="fileUpload" type="file" onChange={(event) => { setNewCertificateUpload(event.target.files[0]); }}/>
     </div>
     <div>
 
     </div>
     <button onClick={handleClick} >Submit</button>
+    <button onClick={nukeDatabase} >NUKE</button>
     </div>
 
   );
