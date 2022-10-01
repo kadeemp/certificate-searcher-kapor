72b72d32a5d147f45047fd2782d804794cb9040c
Kadeem Palacios <kadeempalacios@gmail.com>
2022-10-01 16:58:27 -0700

    tested out anoter pdf rendering framework. Similar errors.

--- src/PDFPage.js
+++ src/PDFPage.js
@@ -1,15 +1,17 @@
 import { Document } from 'react-pdf'
 import { Page, Text, View, StyleSheet } from '@react-pdf/renderer';
+import { Worker, Viewer } from '@react-pdf-viewer/core';
+
 
 function PDFPage() {
 
   return (
-    <div>
-      <Document file={{
-      url:
-        'https://firebasestorage.googleapis.com/v0/b/kapor-center-cert-search.appspot.com/o/cert-pdfs%2F%23000001.pdf?alt=media&token=94b5fb97-c266-49dc-a203-8e1d2a9328f7',
-    }} onLoadError={console.error} >
-      </Document>
+    <div style= {{height:550}}>
+    <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.5.207/build/pdf.worker.min.js">
+     <Viewer fileUrl="https://firebasestorage.googleapis.com/v0/b/kapor-center-cert-search.appspot.com/o/cert-pdfs%2F%23000001.pdf?alt=media&token=94b5fb97-c266-49dc-a203-8e1d2a9328f7" />
+
+    ...
+    </Worker>
     </div>
   );
 }
