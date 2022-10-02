8f2a111c811221a4a70498e57259ee6a097a82ae
Kadeem Palacios <kadeempalacios@gmail.com>
2022-10-01 19:12:19 -0700

    testpush

--- src/PDFPage.js
+++ src/PDFPage.js
@@ -1,17 +1,16 @@
-import { Document } from 'react-pdf'
+import { Document, pdfjs } from 'react-pdf'
 import { Page, Text, View, StyleSheet } from '@react-pdf/renderer';
-import { Worker, Viewer } from '@react-pdf-viewer/core';
-
 
 function PDFPage() {
+  pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
   return (
-    <div style= {{height:550}}>
-    <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.5.207/build/pdf.worker.min.js">
-     <Viewer fileUrl="https://firebasestorage.googleapis.com/v0/b/kapor-center-cert-search.appspot.com/o/cert-pdfs%2F%23000001.pdf?alt=media&token=94b5fb97-c266-49dc-a203-8e1d2a9328f7" />
-
-    ...
-    </Worker>
+    <div>
+      <Document file={{
+      url:
+        'https://firebasestorage.googleapis.com/v0/b/kapor-center-cert-search.appspot.com/o/cert-pdfs%2F%23000001.pdf?alt=media&token=94b5fb97-c266-49dc-a203-8e1d2a9328f7',
+    }} onLoadError={console.error} >
+      </Document>
     </div>
   );
 }
